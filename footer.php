45f33377beb0995ffa8bce5ac012367dfabf1cfe
micalexander <micalexander@hotmail.com>
2013-09-18 16:03:39 -0700

    edit to script in footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
 	</div> <!-- Close .wrapper !-->
 
-<?php wp_enqueue_script( 'js-combo', get_bloginfo( 'template_directory' ) . '/js/public/script-min.js', array('jquery')); ?>
+<?php wp_enqueue_script( 'js-combo', get_bloginfo( 'template_directory' ) . '/js/public/script.js', array('jquery')); ?>
 
 <?php wp_footer(); ?><!-- required don't remove -->
 
