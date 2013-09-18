22170ceea36a67c104c01969a6f9f877fbab370e
micalexander <micalexander@hotmail.com>
2013-09-18 14:45:42 -0700

    added a public folder in the js folder allowinf for guard to minify js to that folder and changed the script tag in the footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 		</div> <!-- End .content -->
-		
+
 		<footer>
 			<p>&copy; <?php echo date("Y"); ?> Company Name.</p>
 		</footer>
 
 	</div> <!-- Close .wrapper !-->
 
-<?php wp_enqueue_script( 'js-combo', get_bloginfo( 'template_directory' ) . '/js/script-min.js', array('jquery')); ?>
+<?php wp_enqueue_script( 'js-combo', get_bloginfo( 'template_directory' ) . '/js/public/script-min.js', array('jquery')); ?>
 
 <?php wp_footer(); ?><!-- required don't remove -->
 
