3764ebff291e8593d06fb672df5295917fad84fb
micalexander <micalexander@hotmail.com>
2013-11-09 12:40:40 -0700

    force flush rewrite once

--- functions.php
+++ functions.php
@@ -204,25 +204,20 @@ function run_activate_plugin( $plugin ) {
 }
 
 function run_options_once() {
-  $check = get_option('mask_activation_check');
+  $check = get_option('newmask_activation_check');
 
 	if ( $check != "set" ) {
 
 		// set permalinks
-	 //    global $wp_rewrite;
-	 //    $wp_rewrite->set_permalink_structure( '/%postname%/' );
-		// $wp_rewrite->flush_rules();
+	    global $wp_rewrite;
+	    $wp_rewrite->set_permalink_structure( '/%postname%/' );
+		$wp_rewrite->flush_rules();
 
 	    // Add marker so it doesn't run in future
-	    add_option('mask_activation_check', "set");
+	    add_option('newmask_activation_check', "set");
 	}
 }
-function setup_permalinks_by_default() {
-    global $wp_rewrite;
-    $wp_rewrite->set_permalink_structure('/%postname%/');
-    $wp_rewrite->flush_rules();
-}
-add_action('after_switch_theme', 'setup_permalinks_by_default');
+ flush_rewrite_rules(true);
 
 add_action('init', 'run_options_once');
 
