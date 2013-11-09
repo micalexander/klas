5141a8817c53460ad717ca0fc7d40c9ea9b692a7
micalexander <micalexander@hotmail.com>
2013-11-09 12:15:02 -0700

    force flush rewrite once

--- functions.php
+++ functions.php
@@ -209,16 +209,16 @@ function run_options_once() {
 	if ( $check != "set" ) {
 
 		// set permalinks
-		add_action( 'init', function() {
-		    global $wp_rewrite;
-		    $wp_rewrite->set_permalink_structure( '/%postname%/' );
-			$wp_rewrite->flush_rules();
-		} );
+	    global $wp_rewrite;
+	    $wp_rewrite->set_permalink_structure( '/%postname%/' );
+		$wp_rewrite->flush_rules();
 
 	    // Add marker so it doesn't run in future
 	    add_option('mask_activation_check', "set");
 	}
 }
+// add_action( 'init', function() {
+// } );
 
 add_action('init', 'run_options_once');
 
