0187a0ed070062794a738f540d85497482c7cab3
micalexander <micalexander@hotmail.com>
2013-11-09 00:21:18 -0700

    edit to menu creation and commented out ie scss mask call

--- functions.php
+++ functions.php
@@ -8,29 +8,33 @@ function mask_scripts_init() {
 }
 add_action('wp_enqueue_scripts', 'mask_scripts_init');
 
-$menu_exists = wp_get_nav_menu_object( 'Primary Navigation' );
+// Add custom navigation to theme and adds Primary Navigation menu
+function mask_menus_init() {
+	menu_exists = wp_get_nav_menu_object( 'Primary Navigation' );
 
-// If it doesn't exist, let's create it.
-if( !$menu_exists){
-    $menu_id = wp_create_nav_menu( 'Primary Navigation', array( 'slug' => 'primary-nav' ) );
+	// If it doesn't exist, let's create it.
+	if( !$menu_exists){
+	    $menu_id = wp_create_nav_menu( 'Primary Navigation' ) );
 
-	// Set up default menu items
-    wp_update_nav_menu_item($menu_id, 0, array(
-        'menu-item-title' =>  __('Home'),
-        'menu-item-classes' => 'home',
-        'menu-item-url' => home_url( '/' ),
-        'menu-item-status' => 'publish'));
+		// Set up default menu items
+	    wp_update_nav_menu_item($menu_id, 0, array(
+	        'menu-item-title' =>  __('Home'),
+	        'menu-item-classes' => 'home',
+	        'menu-item-url' => home_url( '/' ),
+	        'menu-item-status' => 'publish'));
 
-    wp_update_nav_menu_item($menu_id, 0, array(
-        'menu-item-title' =>  __('About us'),
-        'menu-item-url' => home_url( '/custom/' ),
-        'menu-item-status' => 'publish'));
+	    wp_update_nav_menu_item($menu_id, 0, array(
+	        'menu-item-title' =>  __('About us'),
+	        'menu-item-url' => home_url( '/custom/' ),
+	        'menu-item-status' => 'publish'));
 
-    wp_update_nav_menu_item($menu_id, 0, array(
-        'menu-item-title' =>  __('Contact us'),
-        'menu-item-url' => home_url( '/custom/' ),
-        'menu-item-status' => 'publish'));
+	    wp_update_nav_menu_item($menu_id, 0, array(
+	        'menu-item-title' =>  __('Contact us'),
+	        'menu-item-url' => home_url( '/custom/' ),
+	        'menu-item-status' => 'publish'));
+	}
 }
+add_action( 'init', 'mask_menus_init' );
 
 // Registers Primary Widget Area
 function mask_widgets_init() {
