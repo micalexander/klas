099b14f61b84dd9d57bd35233f9c7952403a3124
micalexander <micalexander@hotmail.com>
2013-11-23 16:25:25 -0700

    edit to responsive nav overides

--- js/framework/responsive-nav.js
+++ js/framework/responsive-nav.js
@@ -1,51 +1,54 @@
 (function() {
 	$(document).ready(function(){
 
+		$('.menu-item').has( "ul" ).each(function(e) {
+	    	if (!$(this).data('appends')) {
+
+				$(this).clone().removeClass('menu-item-has-children').addClass('cloned').prependTo($(this).children('ul.sub-menu'));
+				$(this).closest('.menu-item').children('ul.sub-menu').children('li').children('ul.sub-menu').remove();
+
+				$(this).data('appends','appended');
+			}
+		});
 		var nav = responsiveNav(".nav-collapse", { // Selector
 		    animate: true, // Boolean: Use CSS3 transitions, true or false
 		    transition: 250, // Integer: Speed of the transition, in milliseconds
-		    label: "Menu", // String: Label for the navigation toggle
-		    insert: "after", // String: Insert the toggle before or after the navigation
-		    customToggle: "", // Selector: Specify the ID of a custom toggle
+		    label: "", // String: Label for the navigation toggle
+		    insert: "before", // String: Insert the toggle before or after the navigation
+		    customToggle: "toggle", // Selector: Specify the ID of a custom toggle
 		    openPos: "static", // String: Position of the opened nav, relative or static
 		    jsClass: "js", // String: 'JS enabled' class which is added to <html> el
 		    init: function(){}, // Function: Init callback
-		    open: function(){}, // Function: Open callback
+		    open: function(){
+	    		if ('.menu-item ul') {
+					$('.sub-menu').hide();
+				}
+
+		    }, // Function: Open callback
 		    close: function(){} // Function: Close callback
 		});
+
 	});
 })();
 
 function enableNav() {
-
-	if ('.menu-item ul') {
-		$('.sub-menu').hide();
-	}
-
-	$('.menu-item a').click(function(e) {
+	$('.menu-item').has( "ul" ).children('a').on('click', function(e) {
 		e.preventDefault();
 		e.stopPropagation();
-		if ($(this).parent('.menu-item:has(> ul)')) {
-			// set clicks to the value of data['clicks'] which is unset
-			var clicks = $(this).data('clicks');
-			// if clicks is not set to true
-			if (!clicks) {
-				if (!$(this).data('appends')) {
-	   				$(this).parent().clone().prependTo($(this).parent().children('ul.sub-menu'));
-	   				$(this).closest('.menu-item').children('ul.sub-menu').children('li').children('ul.sub-menu').remove();
-				};
-    			$(this).parent().children('ul.sub-menu').slideDown();
-    			// $(this).closest('.menu-item').children('.sub-menu').slideDown();
-    		} else {
-    			$(this).parent().children('ul.sub-menu').slideUp();
-			}
-			// set data['clicks'] to the opposite of its value
-			$(this).data('clicks', !clicks).data('appends','appended');
-    	}
+		// set clicks to the value of data['clicks'] which is unset
+		var clicks = $(this).data('clicks');
+		// if clicks is not set to true
+		if (!clicks) {
+			$(this).parent().children('ul.sub-menu').first().slideDown(150);
+		} else {
+			$(this).parent().children('ul.sub-menu').first().slideUp(150);
+		}
+		// set data['clicks'] to the opposite of its current value
+		$(this).data('clicks', !clicks);
 	})
 }
 
 function disableNav() {
-	$('.menu-item a').off();
+	$('.menu-item').has( "ul" ).children('a').off('click');
 	$('.sub-menu').css({ 'display': '' });
 }
