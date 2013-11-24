cb5990f912e20aed5a6e37f77dbf3d08c54e8055
micalexander <micalexander@hotmail.com>
2013-11-24 08:31:27 -0700

    changed archive type name

--- inc/gridpress/archive.php
+++ inc/gridpress/archive.php
@@ -1,8 +1,8 @@
 <?php
-	$archive = get_sub_field('archive');
+
 	$archive_unit_span = get_sub_field('archive_unit_span');
-	$post_type = get_sub_field('post_type');
-	$post_per_page = get_sub_field('post_per_page');
+	$archive_post_type = get_sub_field('post_type');
+	$archive_post_per_page = get_sub_field('post_per_page');
 	$filter_by =  get_sub_field('filter_by');
 	$elements = get_sub_field('elements');
 
@@ -12,9 +12,9 @@
 		{
 			require('filter-by-last-name.php');
 		}
-		elseif ($filter_by == 'month')
+		elseif ($filter_by == 'start_date')
 		{
-			require('filter-by-month.php');
+			require('filter-by-start-date.php');
 		}
 	}
 ?>
